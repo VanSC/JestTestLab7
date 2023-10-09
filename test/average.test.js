ec54995fbc27fd0eb2d22872a3452895af7fc9bd
VanSC <vanessa.choque@tecsup.edu.pe>
2023-10-09 11:01:59 -0500

    add test in average

--- test/average.test.js
+++ test/average.test.js
@@ -7,4 +7,7 @@ describe('average', () =>{
     test('of one value is the value itself', () => {
         expect(average([1,2,3,4,5,6])).toBe(3.5)
     })
+    test('of one value is the value itself', () => {
+        expect(average([2,2])).toBe(2)
+    })
 })
